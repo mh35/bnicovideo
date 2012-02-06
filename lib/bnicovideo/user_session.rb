30ed5566a77d45c70340004ae6f1a64c4aa5ef76
MH35 <mh35jp@gmail.com>
2012-02-07 07:33:36 +0900

    Chrome cookie

--- lib/bnicovideo/user_session.rb
+++ lib/bnicovideo/user_session.rb
@@ -14,5 +14,13 @@ module Bnicovideo
     def self.init_from_firefox
       ini_path = File.join(ENV['APPDATA'], 'Mozilla', 'Firefox', 'profiles.ini')
     end
+    def self.init_from_chrome
+      sql_path = File.join(ENV['LOCALAPPDATA'], 'Google','Chrome', 'User Data', 'Default', 'Cookies')
+      conn = SQLite3::Database.new(sql_path)
+      val = conn.get_first_value("select value from cookies" +
+          " where host_key='.nicovideo.jp' AND name='user_session'")
+      conn.close
+      return self.new(val)
+    end
   end
 end
