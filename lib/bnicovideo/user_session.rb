7ae1da4e9718ab5f2734c8d6794c2ec6305cbff2
honda <honda@honda-PC>
2012-02-10 07:54:18 +0900

    Where the cookies are

--- lib/bnicovideo/user_session.rb
+++ lib/bnicovideo/user_session.rb
@@ -25,7 +25,11 @@ module Bnicovideo
         else
           profile_path = input_path
         end
-        cookie_path = File.join(profile_path, 'cookies.sqlite')
+        sql_path = File.join(profile_path, 'cookies.sqlite')
+        conn = SQLite3::Database.new(sql_path)
+        val = conn.get_first_value("select value from moz_cookies" +
+            " where host='.nicovideo.jp' AND name='user_session'")
+        return self.new(val)
       end
     end
     def self.init_from_chrome
@@ -35,6 +39,17 @@ module Bnicovideo
           " where host_key='.nicovideo.jp' AND name='user_session'")
       conn.close
       return self.new(val)
+    end
+    def self.init_from_ie
+      cookies_path = File.join(ENV['APPDATA'], 'Microsoft', 'Windows', 'Cookies', 'Low')
+    end
+    def self.init_from_opera
+      cookie_path = File.join(ENV['APPDATA'], 'Opera', 'Opera', 'profile', 'cookies4.dat')
+    end
+    def self.init_from_safari
+      cookie_path = File.join(ENV['APPDATA'], 'Apple Computer', 'Safari',
+        'Cookies', 'Cookies.binarycookies')
+      raise NotImplementedError, 'I don\'t know how to resolve ' + cookie_path
     end
   end
 end
